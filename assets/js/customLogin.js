fb3560ea3708dff4e26ac8a670690e6c104f1fad
Kartik <kartik.arora1214@gmail.com>
2021-04-06 07:49:54 +0530

    OTP based login and account info

--- assets/js/customLogin.js
+++ assets/js/customLogin.js
@@ -1,37 +1,69 @@
+let OTP_endpoint = `http://rusktvapi-dev.akriya.co.in/`;
+function onRequestOTP(e) {
+  e.preventDefault();
+  e.target.disabled = true;
+  console.log('Not redirecting');
+  let phonenumber = document.getElementById('phonenumber').value;
+  let otpForm = document.getElementById('otp-form');
+  let data = {
+    callingParty: phonenumber
+  };
+  console.log(data)
+  fetch(`${OTP_endpoint}/requestOTP`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data)
+  })
+    .then(data => {
+      console.log(data.json());
+    })
+  otpForm.hidden = false;
+}
 
-    function onRequestOTP(e) {
-        e.preventDefault();
-        e.target.disabled = true;
-        console.log('Not redirecting');
-        let phonenumber = document.getElementById('phonenumber').value;
-        let otpForm = document.getElementById('otp-form');
-        otpForm.hidden = false;
-    }
+function verifyOTP(e) {
 
-    function verifyOTP(e) {
+  let phonenumber = document.getElementById('phonenumber').value;
+  let otpValue = document.getElementById('otp-input').value;
+  let data = {
+    callingParty: phonenumber,
+    otpValue: otpValue
+  };
+  console.log(`Verify OTP request ${phonenumber} | ${otpValue}`);
+  e.preventDefault();
+  fetch(`${OTP_endpoint}/confirmOTP`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then((response) => {
+      console.log(response);
+      if (response.isVaidOTP) {
+        letUserKnowSomething('OTP Verified. Redirecting..');
+        localStorage.setItem('kw_logged_data', JSON.stringify(response.data));
+        setTimeout(() => { window.location.href = '/' }, 1000);
 
-      console.log(`Verify OTP request`);
-      e.preventDefault();
-      fetch('OTP_VERIFY_API_ENDPOINT')
-        .then(response => response.json)
-        .then((response) => {
-          if (response.isVaidOTP) {
+      } else {
+        letUserKnowSomething('OTP seems to be invalid. Please try again.');
+        document.getElementById('requestOTPBtn').disabled = false;
 
-          } else {
-            letUserKnowSomething('OTP seems to be invalid. Please try again.'); 
-            document.getElementById('requestOTPBtn').disabled = false;
+      }
+    })
 
-          }
-        })
-      
-    }
+}
 
-    function requestOTPfirebaseFunctionTrigger(phoneNumber) {
-        
-    }
+function requestOTPfirebaseFunctionTrigger(phoneNumber) {
 
-    function letUserKnowSomething(text) {
-      document.getElementById('sendUserInfo').innerHTML = `
+}
+
+function letUserKnowSomething(text) {
+  document.getElementById('sendUserInfo').innerHTML = `
         <p> ${text} </p>
         `
-    }
+}
