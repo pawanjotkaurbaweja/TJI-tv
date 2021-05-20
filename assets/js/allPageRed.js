5af0e1cc4369c4898ab8fe40b817311196a7fbcf
Kartik <kartik.arora1214@gmail.com>
2021-05-20 14:56:28 +0530

    remove auth

--- assets/js/allPageRed.js
+++ assets/js/allPageRed.js
@@ -1,17 +1,17 @@
-function checkIfUserLoggedInOrRedirect() {
-    let lastLocalstorageValue = localStorage.getItem('kw_logged_data');
-    console.log(lastLocalstorageValue);
+// function checkIfUserLoggedInOrRedirect() {
+//     let lastLocalstorageValue = localStorage.getItem('kw_logged_data');
+//     console.log(lastLocalstorageValue);
 
-    if(lastLocalstorageValue == undefined) {
-        //user needs to be redirected;
-        // console.log(`user will be redirected`);
-        if(window.location.href.indexOf('/account') < 0 && window.location.href.indexOf('/login') < 0) {  //only if not already on login or account page
-            window.location.href = '/account/'
-        }
-    } else {
-        // console.log(`We have user data. Let's update relevent DOM`);
-    }
-}
+//     if(lastLocalstorageValue == undefined) {
+//         //user needs to be redirected;
+//         // console.log(`user will be redirected`);
+//         if(window.location.href.indexOf('/account') < 0 && window.location.href.indexOf('/login') < 0) {  //only if not already on login or account page
+//             window.location.href = '/account/'
+//         }
+//     } else {
+//         // console.log(`We have user data. Let's update relevent DOM`);
+//     }
+// }
 
-setInterval(checkIfUserLoggedInOrRedirect, 10000)       //For PROD
-// checkIfUserLoggedInOrRedirect();                     // Only to test
+// setInterval(checkIfUserLoggedInOrRedirect, 10000)       //For PROD
+// // checkIfUserLoggedInOrRedirect();                     // Only to test
