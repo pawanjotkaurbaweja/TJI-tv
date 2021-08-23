4b4ef86b4bc3aae2bafa5d0419cbfb741d3d9d08
Kartik <kartik.arora1214@gmail.com>
2021-08-24 03:01:57 +0530

    no redirect on plan pages

--- assets/js/allPageRed.js
+++ assets/js/allPageRed.js
@@ -7,6 +7,7 @@ function checkIfUserLoggedInOrRedirect() {
         // console.log(`user will be redirected`);
         if (window.location.href.indexOf('/account') < 0
             && window.location.href.indexOf('/login') < 0
+            && window.location.href.indexOf('/plan') < 0
             && window.location.href.indexOf('/sub') < 0) {  //only if not already on login or account page
             window.location.href = '/sub/'
         }
